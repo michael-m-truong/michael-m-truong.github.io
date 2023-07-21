93707ec7b0ed1bf0c1936ccf030ee9ca7fbe4dec
Michael Truong <truong.minh.michael@gmail.com>
2023-07-21 00:48:25 -0700

    added slideshow for imgs

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,11 +67,11 @@ for (let i = 0; i < testimonialsItem.length; i++) {
   });
 
 }
-
+*/
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);  */
+overlay.addEventListener("click", testimonialsModalFunc);
 
 
 
@@ -194,4 +194,34 @@ for (let i = 0; i < navigationLinks.length; i++) {
     }
 
   });
+}
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-slide", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active-slide";
 }
